8d8aebe667108ba088ee806fdd6671c408cbfb1a
Valera <v.medovkin1664@gmail.com>
2022-06-07 00:36:16 +0300

    add binary search

---
+++ binary_search.py
@@ -0,0 +1,24 @@
+def binary_search(arr: list, item: int, low=None, high=None) -> int | None:
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(arr) - 1
+
+    while low <= high:
+        middle = (low + high) // 2
+        middle_value = arr[middle]
+
+        if middle_value == item:
+            return middle
+
+        elif middle_value > item:
+            return binary_search(arr, item, low, middle - 1)
+
+        elif middle_value < item:
+            return binary_search(arr, item, middle + 1, high)
+
+    return None
+
+
+lst = [331, 893, 656, 176, 172, 104, 938, 202, 580, 620]
+print(binary_search(lst, 580))
